439a96cd6d508326ce711f09548420e61d147df5
DucVan <nguyenducvan260903@gmail.com>
2025-02-27 15:46:15 +0700

    refactor: enforce non-null constraints on fields #10

--- Database/RealEstate.sql
+++ Database/RealEstate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 27, 2025 lúc 05:28 AM
+-- Thời gian đã tạo: Th2 27, 2025 lúc 09:41 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -31,7 +31,7 @@ USE `real_estate`;
 
 CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
-  `name` enum('LAND','HOUSE') DEFAULT NULL
+  `name` enum('LAND','HOUSE') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -42,8 +42,8 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `favouriteproperties` (
   `favourite_property_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `property_id` int(11) DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -54,11 +54,11 @@ CREATE TABLE `favouriteproperties` (
 
 CREATE TABLE `houses` (
   `house_id` int(11) NOT NULL,
-  `property_id` int(11) DEFAULT NULL,
-  `bedrooms` int(11) DEFAULT NULL,
-  `toilets` int(11) DEFAULT NULL,
-  `furnished_status` enum('HIGH_END_FURNITURE','FULLY_FURNISHED','BASIC_FINISHING','RAW_HANDOVER') DEFAULT NULL,
-  `house_characteristics` enum('CAR_ACCESS_ALLEY','EXPANDED_HOUSE','NARROWED_HOUSE','PLANNING_AFFECTED_HOUSE','UNCOMPLETED_HOUSE','DAMAGED_HOUSE','NON_RESIDENTIAL_LAND') DEFAULT NULL
+  `property_id` int(11) NOT NULL,
+  `bedrooms` int(11) NOT NULL,
+  `toilets` int(11) NOT NULL,
+  `furnished_status` enum('HIGH_END_FURNITURE','FULLY_FURNISHED','BASIC_FINISHING','RAW_HANDOVER') NOT NULL,
+  `house_characteristics` enum('CAR_ACCESS_ALLEY','EXPANDED_HOUSE','NARROWED_HOUSE','PLANNING_AFFECTED_HOUSE','UNCOMPLETED_HOUSE','DAMAGED_HOUSE','NON_RESIDENTIAL_LAND') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,9 +69,9 @@ CREATE TABLE `houses` (
 
 CREATE TABLE `lands` (
   `land_id` int(11) NOT NULL,
-  `property_id` int(11) DEFAULT NULL,
-  `type_of_land` enum('RESIDENTIAL_LAND','PROJECT_LAND','INDUSTRIAL_LAND','AGRICULTURAL_LAND') DEFAULT NULL,
-  `land_characteristics` enum('FRONTAGE','BACK_EXPANSION','PARTIAL_RESIDENTIAL','NO_RESIDENTIAL','CAR_ALLEY','ALL_RESIDENTIAL') DEFAULT NULL
+  `property_id` int(11) NOT NULL,
+  `type_of_land` enum('RESIDENTIAL_LAND','PROJECT_LAND','INDUSTRIAL_LAND','AGRICULTURAL_LAND') NOT NULL,
+  `land_characteristics` enum('FRONTAGE','BACK_EXPANSION','PARTIAL_RESIDENTIAL','NO_RESIDENTIAL','CAR_ALLEY','ALL_RESIDENTIAL') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -82,23 +82,23 @@ CREATE TABLE `lands` (
 
 CREATE TABLE `properties` (
   `property_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `category_id` int(11) DEFAULT NULL,
-  `status` enum('PENDING','APPROVAL','CANCELED') DEFAULT 'PENDING',
-  `title` varchar(100) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `region` varchar(50) DEFAULT NULL,
-  `ward_name` varchar(50) DEFAULT NULL,
-  `street_name` varchar(100) DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `latitude` double DEFAULT NULL,
-  `property_legal_document` enum('HAS_PAPER','WAITING_FOR_PAPER','NO_PAPER','SHARED_PAPER','HANDWRITTEN_DOCUMENT') DEFAULT NULL,
-  `direction` varchar(50) DEFAULT NULL,
-  `area` double DEFAULT NULL,
-  `length` double DEFAULT NULL,
-  `width` double DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `status` enum('PENDING','APPROVAL','CANCELED') NOT NULL DEFAULT 'PENDING',
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `ward_name` varchar(50) NOT NULL,
+  `street_name` varchar(100) NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL,
+  `property_legal_document` enum('HAS_PAPER','WAITING_FOR_PAPER','NO_PAPER','SHARED_PAPER','HANDWRITTEN_DOCUMENT') NOT NULL,
+  `direction` varchar(50) NOT NULL,
+  `area` double NOT NULL,
+  `length` double NOT NULL,
+  `width` double NOT NULL,
   `images` text DEFAULT NULL,
-  `price` decimal(15,2) DEFAULT NULL,
+  `price` decimal(15,2) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -111,8 +111,17 @@ CREATE TABLE `properties` (
 
 CREATE TABLE `roles` (
   `role_id` int(11) NOT NULL,
-  `name` enum('CUSTOMER','BROKER','ADMIN') DEFAULT 'CUSTOMER'
+  `name` enum('CUSTOMER','BROKER','ADMIN') NOT NULL DEFAULT 'CUSTOMER'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `name`) VALUES
+(1, 'ADMIN'),
+(2, 'BROKER'),
+(3, 'CUSTOMER');
 
 -- --------------------------------------------------------
 
@@ -122,9 +131,9 @@ CREATE TABLE `roles` (
 
 CREATE TABLE `tokens` (
   `token_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `expires_at` datetime DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires_at` datetime NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -136,9 +145,9 @@ CREATE TABLE `tokens` (
 
 CREATE TABLE `transactionshistory` (
   `transaction_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `transaction_type` enum('UPGRADE') DEFAULT NULL,
-  `amount` decimal(15,2) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `transaction_type` enum('UPGRADE') NOT NULL,
+  `amount` decimal(15,2) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -151,15 +160,24 @@ CREATE TABLE `transactionshistory` (
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `role_id` int(11) DEFAULT NULL,
+  `role_id` int(11) NOT NULL DEFAULT 3,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `phone` varchar(15) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`user_id`, `role_id`, `username`, `email`, `password`, `phone`, `address`, `created_at`, `updated_at`) VALUES
+(1, 1, 'user1', 'user1@example.com', 'hashed_password_1', '0123456789', '123 Main St', '2025-02-27 07:52:47', '2025-02-27 07:52:47'),
+(2, 2, 'broker1', 'broker1@example.com', 'hashed_password_2', '0987654321', '456 Elm St', '2025-02-27 07:52:47', '2025-02-27 07:52:47'),
+(3, 3, 'admin1', 'admin1@example.com', 'hashed_password_3', '0112233445', '789 Oak St', '2025-02-27 07:52:47', '2025-02-27 07:52:47');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -268,7 +286,7 @@ ALTER TABLE `properties`
 -- AUTO_INCREMENT cho bảng `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `tokens`
@@ -286,7 +304,7 @@ ALTER TABLE `transactionshistory`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
