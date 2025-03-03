5dfd1ef3c7c04897847d13f134a491af10062279
DucVan <nguyenducvan260903@gmail.com>
2025-03-03 11:53:43 +0700

    chore: rename tables favouriteproperties and transactionshistory with underscores #10

--- Database/RealEstate.sql
+++ Database/RealEstate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 27, 2025 lúc 09:41 AM
+-- Thời gian đã tạo: Th3 03, 2025 lúc 05:53 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -37,363 +37,10 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `favouriteproperties`
+-- Cấu trúc bảng cho bảng `favourite_properties`
 --
 
-CREATE TABLE `favouriteproperties` (
-  `favourite_property_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `property_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `houses`
---
-
-CREATE TABLE `houses` (
-  `house_id` int(11) NOT NULL,
-  `property_id` int(11) NOT NULL,
-  `bedrooms` int(11) NOT NULL,
-  `toilets` int(11) NOT NULL,
-  `furnished_status` enum('HIGH_END_FURNITURE','FULLY_FURNISHED','BASIC_FINISHING','RAW_HANDOVER') NOT NULL,
-  `house_characteristics` enum('CAR_ACCESS_ALLEY','EXPANDED_HOUSE','NARROWED_HOUSE','PLANNING_AFFECTED_HOUSE','UNCOMPLETED_HOUSE','DAMAGED_HOUSE','NON_RESIDENTIAL_LAND') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `lands`
---
-
-CREATE TABLE `lands` (
-  `land_id` int(11) NOT NULL,
-  `property_id` int(11) NOT NULL,
-  `type_of_land` enum('RESIDENTIAL_LAND','PROJECT_LAND','INDUSTRIAL_LAND','AGRICULTURAL_LAND') NOT NULL,
-  `land_characteristics` enum('FRONTAGE','BACK_EXPANSION','PARTIAL_RESIDENTIAL','NO_RESIDENTIAL','CAR_ALLEY','ALL_RESIDENTIAL') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `properties`
---
-
-CREATE TABLE `properties` (
-  `property_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `status` enum('PENDING','APPROVAL','CANCELED') NOT NULL DEFAULT 'PENDING',
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `region` varchar(50) NOT NULL,
-  `ward_name` varchar(50) NOT NULL,
-  `street_name` varchar(100) NOT NULL,
-  `longitude` double NOT NULL,
-  `latitude` double NOT NULL,
-  `property_legal_document` enum('HAS_PAPER','WAITING_FOR_PAPER','NO_PAPER','SHARED_PAPER','HANDWRITTEN_DOCUMENT') NOT NULL,
-  `direction` varchar(50) NOT NULL,
-  `area` double NOT NULL,
-  `length` double NOT NULL,
-  `width` double NOT NULL,
-  `images` text DEFAULT NULL,
-  `price` decimal(15,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `roles`
---
-
-CREATE TABLE `roles` (
-  `role_id` int(11) NOT NULL,
-  `name` enum('CUSTOMER','BROKER','ADMIN') NOT NULL DEFAULT 'CUSTOMER'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `roles`
---
-
-INSERT INTO `roles` (`role_id`, `name`) VALUES
-(1, 'ADMIN'),
-(2, 'BROKER'),
-(3, 'CUSTOMER');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `tokens`
---
-
-CREATE TABLE `tokens` (
-  `token_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `expires_at` datetime NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `transactionshistory`
---
-
-CREATE TABLE `transactionshistory` (
-  `transaction_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `transaction_type` enum('UPGRADE') NOT NULL,
-  `amount` decimal(15,2) NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL DEFAULT 3,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `users`
---
-
-INSERT INTO `users` (`user_id`, `role_id`, `username`, `email`, `password`, `phone`, `address`, `created_at`, `updated_at`) VALUES
-(1, 1, 'user1', 'user1@example.com', 'hashed_password_1', '0123456789', '123 Main St', '2025-02-27 07:52:47', '2025-02-27 07:52:47'),
-(2, 2, 'broker1', 'broker1@example.com', 'hashed_password_2', '0987654321', '456 Elm St', '2025-02-27 07:52:47', '2025-02-27 07:52:47'),
-(3, 3, 'admin1', 'admin1@example.com', 'hashed_password_3', '0112233445', '789 Oak St', '2025-02-27 07:52:47', '2025-02-27 07:52:47');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Chỉ mục cho bảng `favouriteproperties`
---
-ALTER TABLE `favouriteproperties`
-  ADD PRIMARY KEY (`favourite_property_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `property_id` (`property_id`);
-
---
--- Chỉ mục cho bảng `houses`
---
-ALTER TABLE `houses`
-  ADD PRIMARY KEY (`house_id`),
-  ADD KEY `property_id` (`property_id`);
-
---
--- Chỉ mục cho bảng `lands`
---
-ALTER TABLE `lands`
-  ADD PRIMARY KEY (`land_id`),
-  ADD KEY `property_id` (`property_id`);
-
---
--- Chỉ mục cho bảng `properties`
---
-ALTER TABLE `properties`
-  ADD PRIMARY KEY (`property_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Chỉ mục cho bảng `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`role_id`);
-
---
--- Chỉ mục cho bảng `tokens`
---
-ALTER TABLE `tokens`
-  ADD PRIMARY KEY (`token_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Chỉ mục cho bảng `transactionshistory`
---
-ALTER TABLE `transactionshistory`
-  ADD PRIMARY KEY (`transaction_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Chỉ mục cho bảng `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `role_id` (`role_id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `categories`
---
-ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `favouriteproperties`
---
-ALTER TABLE `favouriteproperties`
-  MODIFY `favourite_property_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `houses`
---
-ALTER TABLE `houses`
-  MODIFY `house_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `lands`
---
-ALTER TABLE `lands`
-  MODIFY `land_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `properties`
---
-ALTER TABLE `properties`
-  MODIFY `property_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `roles`
---
-ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT cho bảng `tokens`
---
-ALTER TABLE `tokens`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `transactionshistory`
---
-ALTER TABLE `transactionshistory`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `favouriteproperties`
---
-ALTER TABLE `favouriteproperties`
-  ADD CONSTRAINT `favouriteproperties_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `favouriteproperties_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`);
-
---
--- Các ràng buộc cho bảng `houses`
---
-ALTER TABLE `houses`
-  ADD CONSTRAINT `houses_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`);
-
---
--- Các ràng buộc cho bảng `lands`
---
-ALTER TABLE `lands`
-  ADD CONSTRAINT `lands_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`);
-
---
--- Các ràng buộc cho bảng `properties`
---
-ALTER TABLE `properties`
-  ADD CONSTRAINT `properties_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `properties_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
-
---
--- Các ràng buộc cho bảng `tokens`
---
-ALTER TABLE `tokens`
-  ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Các ràng buộc cho bảng `transactionshistory`
---
-ALTER TABLE `transactionshistory`
-  ADD CONSTRAINT `transactionshistory_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-
---
--- Các ràng buộc cho bảng `users`
---
-ALTER TABLE `users`-- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 03, 2025 lúc 05:32 AM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.1.17
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Cơ sở dữ liệu: `real_estate`
---
-CREATE DATABASE IF NOT EXISTS `real_estate` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `real_estate`;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `categories`
---
-
-CREATE TABLE `categories` (
-  `category_id` int(11) NOT NULL,
-  `name` enum('LAND','HOUSE') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `favouriteproperties`
---
-
-CREATE TABLE `favouriteproperties` (
+CREATE TABLE `favourite_properties` (
   `favourite_property_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `property_id` int(11) NOT NULL
@@ -626,10 +273,10 @@ CREATE TABLE `tokens` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `transactionshistory`
+-- Cấu trúc bảng cho bảng `transactions_history`
 --
 
-CREATE TABLE `transactionshistory` (
+CREATE TABLE `transactions_history` (
   `transaction_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `transaction_type` enum('UPGRADE') NOT NULL,
@@ -676,9 +323,9 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Chỉ mục cho bảng `favouriteproperties`
+-- Chỉ mục cho bảng `favourite_properties`
 --
-ALTER TABLE `favouriteproperties`
+ALTER TABLE `favourite_properties`
   ADD PRIMARY KEY (`favourite_property_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `property_id` (`property_id`);
@@ -768,9 +415,9 @@ ALTER TABLE `tokens`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Chỉ mục cho bảng `transactionshistory`
+-- Chỉ mục cho bảng `transactions_history`
 --
-ALTER TABLE `transactionshistory`
+ALTER TABLE `transactions_history`
   ADD PRIMARY KEY (`transaction_id`),
   ADD KEY `user_id` (`user_id`);
 
@@ -794,9 +441,9 @@ ALTER TABLE `categories`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `favouriteproperties`
+-- AUTO_INCREMENT cho bảng `favourite_properties`
 --
-ALTER TABLE `favouriteproperties`
+ALTER TABLE `favourite_properties`
   MODIFY `favourite_property_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -872,9 +519,9 @@ ALTER TABLE `tokens`
   MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `transactionshistory`
+-- AUTO_INCREMENT cho bảng `transactions_history`
 --
-ALTER TABLE `transactionshistory`
+ALTER TABLE `transactions_history`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -888,11 +535,11 @@ ALTER TABLE `users`
 --
 
 --
--- Các ràng buộc cho bảng `favouriteproperties`
+-- Các ràng buộc cho bảng `favourite_properties`
 --
-ALTER TABLE `favouriteproperties`
-  ADD CONSTRAINT `favouriteproperties_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `favouriteproperties_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`);
+ALTER TABLE `favourite_properties`
+  ADD CONSTRAINT `favourite_properties_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `favourite_properties_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`);
 
 --
 -- Các ràng buộc cho bảng `houses`
@@ -937,22 +584,15 @@ ALTER TABLE `tokens`
   ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
--- Các ràng buộc cho bảng `transactionshistory`
+-- Các ràng buộc cho bảng `transactions_history`
 --
-ALTER TABLE `transactionshistory`
-  ADD CONSTRAINT `transactionshistory_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+ALTER TABLE `transactions_history`
+  ADD CONSTRAINT `transactions_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Các ràng buộc cho bảng `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
 COMMIT;
 
