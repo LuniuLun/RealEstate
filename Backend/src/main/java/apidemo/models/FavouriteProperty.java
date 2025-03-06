d6091f816b30eabb99b2887cc1ed825831ecfef1
DucVan <nguyenducvan260903@gmail.com>
2025-03-06 17:04:36 +0700

    feat: add Category entity with JPA mappings and enum for category name #12

---
+++ Backend/src/main/java/apidemo/models/FavouriteProperty.java
@@ -0,0 +1,50 @@
+
+package apidemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favourite_properties")
+public class FavouriteProperty {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "favourite_property_id")
+  private Integer favouritePropertyId;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "property_id", nullable = false)
+  @JsonIgnore
+  private Property property;
+
+  // Getters and Setters
+  public Integer getFavouritePropertyId() {
+    return favouritePropertyId;
+  }
+
+  public void setFavouritePropertyId(Integer favouritePropertyId) {
+    this.favouritePropertyId = favouritePropertyId;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Property getProperty() {
+    return property;
+  }
+
+  public void setProperty(Property property) {
+    this.property = property;
+  }
+}
