ed59c6d8b2e3ca2d444fb24c1ed6884fc6b8a727
DucVan <nguyenducvan260903@gmail.com>
2025-03-06 17:04:45 +0700

    feat: add FurnishedStatus entity with JPA mappings and house relationship #12

---
+++ Backend/src/main/java/apidemo/models/FurnishedStatus.java
@@ -0,0 +1,48 @@
+package apidemo.models;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "furnished_statuses")
+public class FurnishedStatus {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "furnished_status_id")
+  private Integer furnishedStatusId;
+
+  @Column(nullable = false, length = 50)
+  private String name;
+
+  @OneToMany(mappedBy = "furnishedStatus")
+  @JsonIgnore
+  private Set<House> houses;
+
+  // Getters and Setters
+  public Integer getFurnishedStatusId() {
+    return furnishedStatusId;
+  }
+
+  public void setFurnishedStatusId(Integer furnishedStatusId) {
+    this.furnishedStatusId = furnishedStatusId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<House> getHouses() {
+    return houses;
+  }
+
+  public void setHouses(Set<House> houses) {
+    this.houses = houses;
+  }
+}
