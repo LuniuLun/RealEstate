68db6e1ef7486a2e432e4dfcddafd5e5b604d641
DucVan <nguyenducvan260903@gmail.com>
2025-03-12 15:57:17 +0700

    refactor: Remove Google Cloud Storage initialization from main application #45

--- Backend/src/main/java/apidemo/SpringbootApplication.java
+++ Backend/src/main/java/apidemo/SpringbootApplication.java
@@ -1,48 +1,11 @@
 package apidemo;
 
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.InputStream;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SpringbootApplication.class, args);
-    initializeGoogleCloudStorage();
-  }
-
-  private static void initializeGoogleCloudStorage() {
-    try (InputStream serviceAccount = SpringbootApplication.class
-        .getClassLoader()
-        .getResourceAsStream("serviceAccountKey.json")) {
-
-      if (serviceAccount == null) {
-        throw new IllegalStateException("Service account key file not found!");
-      }
-
-      GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-      Storage storage = StorageOptions.newBuilder()
-          .setCredentials(credentials)
-          .setProjectId("rare-animals")
-          .build()
-          .getService();
-
-      listBuckets(storage);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private static void listBuckets(Storage storage) {
-    Page<Bucket> bucketsPage = storage.list();
-    for (Bucket bucket : bucketsPage.iterateAll()) {
-      System.out.println("Bucket name: " + bucket.getName());
-    }
   }
 }
