42aaf6210ea65ed66d4f83a3df9cb67c4440874a
DucVan <nguyenducvan260903@gmail.com>
2025-03-18 10:46:57 +0700

    refactor: enhance app setup with theme, query client, and context

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,15 +5,29 @@ import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/500.css'
 import '@fontsource/open-sans/600.css'
 import '@fontsource/open-sans/700.css'
+import { defaultTheme } from './styles'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
-import { system } from './styles'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SidebarProvider } from '@layout/components/Sidebar/SidebarProvider'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider value={system}>
-        <App />
+      <ChakraProvider theme={defaultTheme}>
+        <QueryClientProvider client={queryClient}>
+          <SidebarProvider>
+            <App />
+          </SidebarProvider>
+        </QueryClientProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
