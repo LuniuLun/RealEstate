245018dde7f94a9ce41e036d60ccdcf3a49c5b58
DucVan <nguyenducvan260903@gmail.com>
2025-03-18 10:53:06 +0700

    style: define brand color palette for UI consistency  #54

---
+++ Frontend/src/styles/variables/colors.ts
@@ -0,0 +1,21 @@
+const colors = {
+  brand: {
+    primary: '#E7C873',
+    secondary: '#EFF4FA',
+    red: '#EF4444',
+    white: '#FFFFFF',
+    black: '#1A1A1A',
+    grey: '#f5f5f5',
+    green: '#47D764',
+    blackTextPrimary: '#222B45',
+    blackTextSecondary: '#8F9BB3',
+    blackTextTertiary: '#757575',
+    blackTextQuaternary: '#717171',
+    whiteBg: '#FFFFFF',
+    blackBg: '#09090B',
+    greyBg: '#192038',
+    hoverBtnColor: '#eec041'
+  }
+}
+
+export default colors
