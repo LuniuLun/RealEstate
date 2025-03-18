13588b78541f6a2a03cf8f0d4bcfb93d763103e9
DucVan <nguyenducvan260903@gmail.com>
2025-03-18 11:36:44 +0700

    feat: add Logo component with customizable icon and link #63

---
+++ Frontend/src/components/Logo/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
+
+interface LogoProps {
+  icon: React.ReactElement
+  src: string
+  width?: string
+  height?: string
+}
+
+const Logo = ({ src, icon, width = '24px', height = '36px' }: LogoProps) => {
+  return (
+    <Link to={src}>
+      <Flex alignItems='center' gap={3}>
+        <Box borderRadius='50%'>{React.cloneElement(icon, { width, height })}</Box>
+        <Heading variant='primary'>JustHome</Heading>
+      </Flex>
+    </Link>
+  )
+}
+
+export default memo(Logo)
