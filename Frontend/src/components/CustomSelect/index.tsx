357666e826a9cd6817de7c95a22adc5a973f4bac
DucVan <nguyenducvan260903@gmail.com>
2025-03-19 08:49:47 +0700

    feat: add CustomSelect component with customizable options and styling #70

---
+++ Frontend/src/components/CustomSelect/index.tsx
@@ -0,0 +1,71 @@
+import { Select, SelectProps } from '@chakra-ui/react'
+import { useState, useEffect, memo } from 'react'
+import { TBorderDirection } from '@type/variant'
+import colors from '@styles/variables/colors'
+import React, { forwardRef } from 'react'
+
+export interface SelectOption<T> {
+  value: T
+  label: string
+}
+
+interface ICustomSelectProps<T> extends SelectProps {
+  options: SelectOption<T>[]
+  border?: TBorderDirection
+  value?: string | number
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+const CustomSelect = forwardRef<HTMLSelectElement, ICustomSelectProps<string | number>>(
+  (
+    { border = 'none', options, placeholder, value, onChange, sx, ...props }: ICustomSelectProps<string | number>,
+    ref
+  ) => {
+    const [selectedValue, setSelectedValue] = useState<string | number>('')
+
+    useEffect(() => {
+      if (value) {
+        setSelectedValue(value)
+      }
+    }, [value])
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const value = e.target.value
+      setSelectedValue(value)
+      if (onChange) {
+        onChange(e)
+      }
+    }
+
+    return (
+      <Select
+        ref={ref}
+        fontSize='sm'
+        fontWeight='medium'
+        value={selectedValue}
+        onChange={handleChange}
+        variant={border === 'bottom' ? 'flushed' : 'filled'}
+        borderRadius={20}
+        sx={{
+          width: 'auto',
+          ...(border === 'full' && { border: `2px solid ${colors.brand.secondary}` }),
+          ...(border === 'bottom' && { borderBottom: `1px solid ${colors.brand.black}` }),
+          _hover: { bgColor: colors.brand.hoverBtnColor, color: colors.brand.white },
+          ...sx
+        }}
+        {...props}
+      >
+        <option value='' disabled hidden>
+          {selectedValue || placeholder || 'Select'}
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
+    )
+  }
+)
+
+export default memo(CustomSelect)
