280768193edf097b2b306e0832ad7178b6e883db
DucVan <nguyenducvan260903@gmail.com>
2025-03-19 11:40:09 +0700

    feat: add useGetProperty hook export #77

--- Frontend/src/hooks/index.ts
+++ Frontend/src/hooks/index.ts
@@ -1,6 +1,9 @@
 export { default as useSidebar } from './UseSidebar'
 export { default as useCustomToast } from './UseCustomToast'
+
 export { default as useGetUser } from './UseUser/useGetUser'
 export { default as useEditUser } from './UseUser/useEditUser'
 export { default as useAddUser } from './UseUser/useAddUser'
 export { default as useDeleteUser } from './UseUser/useDeleteUser'
+
+export { default as useGetProperty } from './UseProperty/useGetProperty'
