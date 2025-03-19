8f38a61b24035abb0ab4715f394de0946d99fb99
DucVan <nguyenducvan260903@gmail.com>
2025-03-19 15:25:03 +0700

    feat: add count methods for status and category in PropertyRepository #79

--- Backend/src/main/java/apidemo/repositories/PropertyRepository.java
+++ Backend/src/main/java/apidemo/repositories/PropertyRepository.java
@@ -6,13 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import apidemo.models.Property;
+import apidemo.models.Property.PropertyStatus;
 
 public interface PropertyRepository extends JpaRepository<Property, Integer>, JpaSpecificationExecutor<Property> {
   List<Property> findByUser_id(int id);
 
   List<Property> findByCategory_id(int id);
 
-  List<Property> findByStatus(String status);
+  List<Property> findByStatus(PropertyStatus status);
 
   long countByUser_id(int id);
+
+  long countByStatus(PropertyStatus status);
+
+  long countByStatusAndCategory_id(PropertyStatus status, int categoryId);
+
+  long countByStatusAndUser_id(PropertyStatus status, int userId);
 }
