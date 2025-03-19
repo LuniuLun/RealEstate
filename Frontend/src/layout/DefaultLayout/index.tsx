b72bb6ee7edabc42acf54b824adbd959eb10dcfc
DucVan <nguyenducvan260903@gmail.com>
2025-03-19 09:15:17 +0700

    feat: add Footer component with contact and social links #58

--- Frontend/src/layout/DefaultLayout/index.tsx
+++ Frontend/src/layout/DefaultLayout/index.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { Flex, Stack } from '@chakra-ui/react'
 import { ErrorBoundary } from '@components'
 import { ClientHeader } from '@layout/components/Header'
+import Footer from '@layout/components/Footer'
 
 const DefaultLayout = () => {
   return (
@@ -10,6 +11,7 @@ const DefaultLayout = () => {
         <Stack w={{ base: '100%' }} paddingBottom={6} bgColor='brand.grey'>
           <ClientHeader />
           <Outlet />
+          <Footer />
         </Stack>
       </Flex>
     </ErrorBoundary>
---
+++ Frontend/src/layout/components/Footer/index.tsx
@@ -0,0 +1,50 @@
+import { FacebookIcon, InstagramIcon, LinkedInIcon, LogoIcon, TwitterIcon } from '@assets/icons'
+import { Box, Container, Flex, Text, Link, VStack, HStack } from '@chakra-ui/react'
+import { Logo } from '@components'
+import colors from '@styles/variables/colors'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Box bg='teal.900' color='white' py={6}>
+      <Container maxW='6xl'>
+        <Flex justify='space-between' align='center' flexWrap='wrap'>
+          <HStack spacing={2}>
+            <Logo icon={<LogoIcon />} src='/' />
+          </HStack>
+
+          <Flex flexDirection={{ base: 'column', md: 'row' }} gap={8}>
+            <VStack align='start'>
+              <Text fontWeight='bold'>Liên hệ tôi</Text>
+              <Link color={colors.brand.blackTextSecondary}>nguyenducvan@gmail.com</Link>
+              <Text color={colors.brand.blackTextSecondary}>0373 115 431</Text>
+            </VStack>
+
+            <VStack align='start'>
+              <Text fontWeight='bold'>Địa chỉ của tôi</Text>
+              <Text color={colors.brand.blackTextSecondary}>79 Dương Bá Cung</Text>
+              <Text color={colors.brand.blackTextSecondary}>Hoà Xuân, Đà Nẵng</Text>
+            </VStack>
+          </Flex>
+
+          <HStack spacing={4}>
+            <Link href='#' isExternal>
+              {React.cloneElement(<FacebookIcon />)}
+            </Link>
+            <Link href='#' isExternal>
+              {React.cloneElement(<InstagramIcon />)}
+            </Link>
+            <Link href='#' isExternal>
+              {React.cloneElement(<TwitterIcon />)}
+            </Link>
+            <Link href='#' isExternal>
+              {React.cloneElement(<LinkedInIcon />)}
+            </Link>
+          </HStack>
+        </Flex>
+      </Container>
+    </Box>
+  )
+}
+
+export default Footer
