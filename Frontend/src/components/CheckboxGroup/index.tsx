aa1bfd1bc958f9df71f58a3c26ae2cbd7d782d3f
DucVan <nguyenducvan260903@gmail.com>
2025-03-20 15:12:05 +0700

    feat: implement checkbox grid for multi-select filtering #80

---
+++ Frontend/src/components/CheckboxGroup/index.tsx
@@ -0,0 +1,30 @@
+import { Checkbox, SimpleGrid } from '@chakra-ui/react'
+import { ISelectOption } from '@components/CustomSelect'
+import colors from '@styles/variables/colors'
+import { CategoryName } from '@type/models'
+
+interface ICheckboxGroupProps {
+  options: ISelectOption<number>[]
+  selectedValues: string[]
+  filterType: string
+  onValueChange: (value: string, filterType: CategoryName) => void
+}
+
+const CheckboxGroup = ({ options, selectedValues, filterType, onValueChange }: ICheckboxGroupProps) => (
+  <SimpleGrid columns={2} spacing={2}>
+    {options.map((option) => (
+      <Checkbox
+        key={option.value}
+        isChecked={selectedValues.includes(String(option.value))}
+        onChange={() => onValueChange(String(option.value), String(filterType) as CategoryName)}
+        colorScheme={colors.brand.primary}
+        size='sm'
+        textAlign='start'
+      >
+        {option.label}
+      </Checkbox>
+    ))}
+  </SimpleGrid>
+)
+
+export default CheckboxGroup
