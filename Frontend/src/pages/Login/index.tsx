22da1bf386299872ff35b349d88252d716ee6448
DucVan <nguyenducvan260903@gmail.com>
2025-03-22 23:48:30 +0700

    pref: apply login form component #85

--- Frontend/src/pages/Login/index.tsx
+++ Frontend/src/pages/Login/index.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, Divider, Flex, Heading, Stack, Text } from '@chakra-ui/react'
-import { GoogleIcon, LogoIcon } from '@assets/icons'
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import { LogoIcon } from '@assets/icons'
 import IllustrationImage from '@assets/images/illustration.png'
-import { Logo, TextField } from '@components'
+import { LoginForm, Logo } from '@components'
 import colors from '@styles/variables/colors'
-import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
@@ -26,50 +25,7 @@ const Login = () => {
       </Flex>
 
       <Box flex={2} p={10} display='flex' justifyContent='center' alignItems='center'>
-        <Stack gap={5} maxW='500px' w='100%'>
-          <Heading fontSize='2xl' color={colors.brand.blackTextPrimary}>
-            Đăng nhập JustHome
-          </Heading>
-
-          <Button
-            variant='primary'
-            bgColor={colors.brand.blue}
-            px='unset'
-            w='300px'
-            height={10}
-            justifyContent='flex-start'
-            borderRadius='sm'
-          >
-            <Box m='2px' borderRadius='md' overflow='hidden'>
-              <GoogleIcon />
-            </Box>
-            Đăng nhập bằng Google
-          </Button>
-
-          <Flex align='center' gap={4}>
-            <Divider my={5} border='1px solid #D9D9D9' />
-            <Text color={colors.brand.yellowHeading}>Hoặc</Text>
-            <Divider my={5} border='1px solid #D9D9D9' />
-          </Flex>
-
-          <Stack gap={4}>
-            <TextField placeholder='Số điện thoại' variant='outline' borderRadius='md' py={5} />
-            <TextField placeholder='Mật khẩu' variant='outline' borderRadius='md' py={5} type='password' />
-          </Stack>
-
-          <Button mt={5} width='250px' colorScheme='yellow' py={5}>
-            Đăng nhập
-          </Button>
-
-          <Text mt={3}>
-            Bạn chưa có tài khoản?{' '}
-            <Link to='/register'>
-              <Text as='span' color={colors.brand.yellowHeading}>
-                Đăng ký
-              </Text>
-            </Link>
-          </Text>
-        </Stack>
+        <LoginForm />
       </Box>
     </Flex>
   )
