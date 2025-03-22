629052e79aea4a74cacae1d1922a40ebc5cdbece
DucVan <nguyenducvan260903@gmail.com>
2025-03-22 23:49:51 +0700

    refactor: update `ProtectedRoute` to use `RoleName` enum  #85

--- Frontend/src/routes/ProtectedRoute.tsx
+++ Frontend/src/routes/ProtectedRoute.tsx
@@ -1,13 +1,19 @@
+import { RoleName } from '@type/models'
 import { Navigate, Outlet } from 'react-router-dom'
 
 type ProtectedRouteProps = {
   isAllowed: boolean
   redirectPath?: string
   requiredRole?: string
-  userRoles?: string[]
+  userRoles?: RoleName
 }
 
-const ProtectedRoute = ({ isAllowed, redirectPath = '/', requiredRole, userRoles = [] }: ProtectedRouteProps) => {
+const ProtectedRoute = ({
+  isAllowed,
+  redirectPath = '/',
+  requiredRole,
+  userRoles = RoleName.CUSTOMER
+}: ProtectedRouteProps) => {
   if (!isAllowed || (requiredRole && !userRoles.includes(requiredRole))) {
     return <Navigate to={redirectPath} replace />
   }
