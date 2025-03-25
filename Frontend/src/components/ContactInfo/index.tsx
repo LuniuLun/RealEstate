e44a820cd56195fd6225f481c0f23118ea84a034
DucVan <nguyenducvan260903@gmail.com>
2025-03-25 09:42:05 +0700

    feat: add ContactInfo component for displaying user contact details #87

---
+++ Frontend/src/components/ContactInfo/index.tsx
@@ -0,0 +1,46 @@
+import { Stack, Flex, Text } from '@chakra-ui/react'
+import { PhoneIcon, UserIcon } from '@assets/icons'
+import UserCard from '@components/UserCard'
+import colors from '@styles/variables/colors'
+import { memo } from 'react'
+
+interface ContactInfoProps {
+  name: string
+  role: string
+  phone: string
+}
+
+const ContactInfo = ({ name, role, phone }: ContactInfoProps) => {
+  return (
+    <Stack
+      w='300px'
+      position='sticky'
+      top='20px'
+      alignSelf='flex-start'
+      gap={4}
+      py={3}
+      px={5}
+      borderRadius='md'
+      bgColor={colors.brand.white}
+      boxShadow='md'
+    >
+      <UserCard name={name} role={role} avatar={<UserIcon />} />
+      <Flex
+        alignItems='center'
+        justifyContent='center'
+        gap={4}
+        px={6}
+        py={3}
+        bgColor={colors.brand.green}
+        borderRadius='md'
+      >
+        <PhoneIcon />
+        <Text color='white' fontSize='lg' fontWeight='bold'>
+          {phone}
+        </Text>
+      </Flex>
+    </Stack>
+  )
+}
+
+export default memo(ContactInfo)
