0528c3925a1e74a60988b4c24245e69953af6ba7
DucVan <nguyenducvan260903@gmail.com>
2025-03-25 11:33:04 +0700

    feat: add input validation to RangeInput component using regex #62

--- Frontend/src/components/RangeInput/index.tsx
+++ Frontend/src/components/RangeInput/index.tsx
@@ -1,5 +1,6 @@
 import { Flex, Input, Text } from '@chakra-ui/react'
 import { Unit } from '@type/models'
+import { REGEX } from '@constants/regex'
 
 interface IRangeInputProps {
   min: string
@@ -9,11 +10,18 @@ interface IRangeInputProps {
 }
 
 const RangeInput = ({ min, max, unit, onChange }: IRangeInputProps) => {
+  const handleInputChange = (key: 'min' | 'max') => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (REGEX.positiveNumber.test(value)) {
+      onChange(key, value)
+    }
+  }
+
   return (
     <Flex gap={2} alignItems='center'>
-      <Input placeholder={'Thấp nhất'} value={min} onChange={(e) => onChange('min', e.target.value)} size='sm' />
+      <Input placeholder='Thấp nhất' value={min} onChange={handleInputChange('min')} size='sm' textAlign='center' />
       <Text>-</Text>
-      <Input placeholder={'Cao nhất'} value={max} onChange={(e) => onChange('max', e.target.value)} size='sm' />
+      <Input placeholder='Cao nhất' value={max} onChange={handleInputChange('max')} size='sm' textAlign='center' />
       <Text>{unit === Unit.BILLION ? 'Tỷ' : 'm²'}</Text>
     </Flex>
   )
--- Frontend/src/constants/regex.ts
+++ Frontend/src/constants/regex.ts
@@ -1,5 +1,6 @@
 export const REGEX = {
   email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
   phone: /^[0-9]{9,11}$/,
-  password: /^(?=.*[A-Z])(?=.*\W).{8,}$/
+  password: /^(?=.*[A-Z])(?=.*\W).{8,}$/,
+  positiveNumber: /^[0-9]+$/
 }
