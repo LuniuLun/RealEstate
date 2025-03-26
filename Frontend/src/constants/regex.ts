f10c0b5139c017c1f7fabf71d829b73d8e149f30
DucVan <nguyenducvan260903@gmail.com>
2025-03-26 16:41:34 +0700

    feat: Add regex for image file extensions in REGEX constants #93

--- Frontend/src/constants/regex.ts
+++ Frontend/src/constants/regex.ts
@@ -2,5 +2,6 @@ export const REGEX = {
   email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
   phone: /^[0-9]{9,11}$/,
   password: /^(?=.*[A-Z])(?=.*\W).{8,}$/,
-  positiveNumber: /^[0-9]+$/
+  positiveNumber: /^[0-9]+$/,
+  imageExtensions: /\.(jpg|jpeg|png|gif|webp)$/i
 }
