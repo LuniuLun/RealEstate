144f4ce0125a5538bedd36c7d1e7e81e62c31b4e
DucVan <nguyenducvan260903@gmail.com>
2025-03-27 16:22:54 +0700

    feat: add currencyFormat regex for number formatting #22

--- Frontend/src/constants/regex.ts
+++ Frontend/src/constants/regex.ts
@@ -3,5 +3,6 @@ export const REGEX = {
   phone: /^[0-9]{9,11}$/,
   password: /^(?=.*[A-Z])(?=.*\W).{8,}$/,
   positiveNumber: /^[0-9]+$/,
-  imageExtensions: /\.(jpg|jpeg|png|gif|webp)$/i
+  imageExtensions: /\.(jpg|jpeg|png|gif|webp)$/i,
+  currencyFormat: /\B(?=(\d{3})+(?!\d))/g
 }
