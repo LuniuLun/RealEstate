1a3ee981755a0ed8e11faef56d75bb1c9125841d
DucVan <nguyenducvan260903@gmail.com>
2025-03-27 15:38:21 +0700

    feat: add useGetCoordinates hook with error handling #99

---
+++ Frontend/src/hooks/UseCoordinates/useGetCoordinates.tsx
@@ -0,0 +1,42 @@
+import MESSAGE from '@constants/message'
+import useCustomToast from '@hooks/UseCustomToast'
+import { getCoordinates } from '@services/coordinates'
+import { useMutation, UseMutationResult } from '@tanstack/react-query'
+import { IApiResponse } from '@type/apiResponse'
+import { Coordinates } from '@type/coordinates'
+
+interface UseGetCoordinatesReturn {
+  getCoordinatesMutation: UseMutationResult<IApiResponse<Coordinates>, Error, string>
+  isLoading: boolean
+  isError: boolean
+}
+
+export const useGetCoordinates = (): UseGetCoordinatesReturn => {
+  const { showToast } = useCustomToast()
+
+  const getCoordinatesMutation = useMutation({
+    mutationFn: getCoordinates,
+    onSuccess: (response) => {
+      if (response && response.status === 'error') {
+        showToast({
+          title: response.message,
+          status: response.status
+        })
+      }
+      console.log(response)
+    },
+    onError: (error) => {
+      console.log(error.message)
+      showToast({
+        title: MESSAGE.coordinates.GET_FAILED,
+        status: 'error'
+      })
+    }
+  })
+
+  return {
+    getCoordinatesMutation,
+    isLoading: getCoordinatesMutation.isPending,
+    isError: getCoordinatesMutation.isError
+  }
+}
