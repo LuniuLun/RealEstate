2a4f6458663cff012e262977bcb14a5d0cb858a4
DucVan <nguyenducvan260903@gmail.com>
2025-03-27 15:43:06 +0700

    feat: add property data conversion hooks for land and house #22

---
+++ Frontend/src/hooks/UseProperty/useConvertProperty.tsx
@@ -0,0 +1,103 @@
+import { LandFormData } from '@components/LandForm'
+import { HouseFormData } from '@components/HouseForm'
+import { authStore } from '@stores'
+import { IUser, TPostProperty } from '@type/models'
+import useValidateToken from '@hooks/UseUser/useValidateToken'
+
+interface UseConvertPropertyDataReturn {
+  convertLandData: (formData: LandFormData) => TPostProperty | null
+  convertHouseData: (formData: HouseFormData) => TPostProperty | null
+}
+
+export const useConvertPropertyData = (): UseConvertPropertyDataReturn => {
+  const { token } = authStore()
+  const validateToken = useValidateToken()
+  const convertLandData = (formData: LandFormData): TPostProperty | null => {
+    if (!validateToken()) return null
+
+    return {
+      user: token?.user as IUser,
+      category: formData.category,
+      title: formData.title,
+      description: formData.description,
+      region: formData.region,
+      districtName: formData.districtName,
+      wardName: formData.wardName,
+      streetName: formData.streetName,
+      longitude: formData.longitude,
+      latitude: formData.longitude,
+      propertyLegalDocument: {
+        id: Number(formData.propertyLegalDocument),
+        name: ''
+      },
+      direction: Number(formData.direction),
+      area: Number(formData.area),
+      length: Number(formData.length),
+      width: Number(formData.width),
+      price: Number(formData.price),
+      land: {
+        landType: {
+          id: formData.landType,
+          name: ''
+        },
+        landCharacteristicMappings:
+          formData.landCharacteristics?.map((id) => ({
+            id: 0,
+            landCharacteristic: {
+              id: id,
+              name: ''
+            }
+          })) || []
+      }
+    }
+  }
+
+  const convertHouseData = (formData: HouseFormData): TPostProperty | null => {
+    if (!validateToken()) return null
+
+    return {
+      user: token?.user as IUser,
+      category: formData.category,
+      title: formData.title,
+      description: formData.description,
+      region: formData.region,
+      districtName: formData.districtName,
+      wardName: formData.wardName,
+      streetName: formData.streetName,
+      longitude: formData.longitude,
+      latitude: formData.longitude,
+      area: Number(formData.area),
+      length: Number(formData.length),
+      width: Number(formData.width),
+      price: Number(formData.price),
+      direction: Number(formData.direction),
+      propertyLegalDocument: {
+        id: Number(formData.propertyLegalDocument),
+        name: ''
+      },
+      house: {
+        houseType: {
+          id: formData.houseType,
+          name: ''
+        },
+        furnishedStatus: {
+          id: formData.furnishedStatus,
+          name: ''
+        },
+        bedrooms: Number(formData.bedrooms),
+        toilets: Number(formData.toilets),
+        floors: Number(formData.floors),
+        houseCharacteristicMappings:
+          formData.houseCharacteristics?.map((id) => ({
+            id: 0,
+            houseCharacteristic: {
+              id: id,
+              name: ''
+            }
+          })) || []
+      }
+    }
+  }
+
+  return { convertLandData, convertHouseData }
+}
