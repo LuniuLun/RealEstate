799b17b1f5bcfaa9bf8fbca366116ffb3ad81197
DucVan <nguyenducvan260903@gmail.com>
2025-04-01 10:55:52 +0700

    refactor: enhance useGetPropertyById with cache support and optional parameters #100

--- Frontend/src/hooks/UseProperty/useGetPropertyById.tsx
+++ Frontend/src/hooks/UseProperty/useGetPropertyById.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, QueryClient } from '@tanstack/react-query'
 import { fetchPropertyById } from '@services/property'
 import { IProperty } from '@type/models'
+import { findPropertyInCache } from '@utils'
 
 interface UseGetPropertyByIdReturn {
   property: IProperty | undefined
@@ -8,10 +9,20 @@ interface UseGetPropertyByIdReturn {
   isError: boolean
 }
 
-const useGetPropertyById = (id: string): UseGetPropertyByIdReturn => {
+const useGetPropertyById = (
+  id: number,
+  queryClient?: QueryClient | null,
+  queryString?: string
+): UseGetPropertyByIdReturn => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['property', id],
-    queryFn: () => fetchPropertyById(id),
+    queryFn: async () => {
+      if (queryClient && id && queryString) {
+        const cacheProperty = findPropertyInCache(queryClient, id, queryString)
+        if (cacheProperty) return { data: cacheProperty }
+      }
+      return await fetchPropertyById(id)
+    },
     enabled: !!id
   })
 
