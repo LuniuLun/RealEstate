0ef496f0f420b96af7dbea1dfe4cf0daa3abb9ee
DucVan <nguyenducvan260903@gmail.com>
2025-04-01 11:01:08 +0700

    chore: update PersonalLayout component structure

--- Frontend/src/layout/PersonalLayout/index.tsx
+++ Frontend/src/layout/PersonalLayout/index.tsx
@@ -3,17 +3,21 @@ import { Flex, Stack } from '@chakra-ui/react'
 import { ErrorBoundary } from '@components'
 import { BaseHeader } from '@layout/components/Header'
 import Sidebar from '@layout/components/Sidebar'
+import Footer from '@layout/components/Footer'
 
 const PersonalLayout = () => {
   return (
     <ErrorBoundary>
-      <Flex gap={4}>
-        <Sidebar />
-        <Stack w={{ base: '100%' }} bgColor='brand.grey' gap='unset'>
-          <BaseHeader />
-          <Outlet />
-        </Stack>
-      </Flex>
+      <Stack>
+        <Flex gap={4}>
+          <Sidebar />
+          <Stack w={{ base: '100%', xl: 'calc(100% - 254px)' }} bgColor='brand.grey' gap='unset' px={3}>
+            <BaseHeader />
+            <Outlet />
+          </Stack>
+        </Flex>
+        <Footer />
+      </Stack>
     </ErrorBoundary>
   )
 }
