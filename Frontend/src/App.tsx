43e1c17b42815b5be7dcb91dfa9be8bddbe17d7a
DucVan <nguyenducvan260903@gmail.com>
2025-04-01 10:44:26 +0700

    refactor: update routes with new property form and my posts section #100

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -10,8 +10,9 @@ import ListingProperty from '@pages/ListingProperty'
 import Register from '@pages/Register'
 import Login from '@pages/Login'
 import DetailPost from '@pages/DetailPost'
-import NewProperty from '@pages/NewProperty'
+import PropertyForm from '@pages/PropertyForm'
 import Profile from '@pages/Profile'
+import MyPosts from '@pages/MyPosts'
 
 function App() {
   const { token } = authStore()
@@ -26,13 +27,14 @@ function App() {
           element={
             <ProtectedRoute
               isAllowed={!!token}
-              userRoles={token?.user?.role?.name || RoleName.CUSTOMER}
-              requiredRole={RoleName.CUSTOMER}
-              redirectPath='/'
+              userRoles={token?.user?.role?.name}
+              requiredRole={[RoleName.CUSTOMER, RoleName.BROKER]}
+              redirectPath='/login'
             />
           }
         >
-          <Route path='new-property' element={<NewProperty />} />
+          <Route path='new-property' element={<PropertyForm />} />
+          <Route path='my-posts/update/:id' element={<PropertyForm />} />
         </Route>
       </Route>
       <Route path='register' element={<Register />} />
@@ -41,20 +43,21 @@ function App() {
         element={
           <ProtectedRoute
             isAllowed={!!token}
-            userRoles={token?.user?.role?.name || RoleName.CUSTOMER}
-            requiredRole={RoleName.CUSTOMER || RoleName.BROKER}
+            userRoles={token?.user?.role?.name}
+            requiredRole={[RoleName.CUSTOMER, RoleName.BROKER]}
             redirectPath='/'
           />
         }
       >
         <Route path='personal' element={<PersonalLayout />}>
           <Route index element={<Profile />} />
+          <Route path='my-posts' element={<MyPosts />} />
           <Route
             element={
               <ProtectedRoute
                 isAllowed={!!token}
-                userRoles={token?.user?.role?.name || RoleName.CUSTOMER}
-                requiredRole={RoleName.ADMIN}
+                userRoles={token?.user?.role?.name}
+                requiredRole={[RoleName.ADMIN]}
                 redirectPath='/'
               />
             }
