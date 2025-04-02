29d99f04ab6eeb9429dabb63f2801b39ec553bdd
DucVan <nguyenducvan260903@gmail.com>
2025-04-02 14:53:11 +0700

    feat: add UserMenu component with user actions and dropdown menu #109

---
+++ Frontend/src/components/UserMenu/index.tsx
@@ -0,0 +1,65 @@
+import { Box, Button, Flex } from '@chakra-ui/react'
+import { LogoutIcon, ProfileIcon, UpgradeIcon, UserIcon } from '@assets/icons'
+import { UserCard } from '@components'
+import { IToken } from '@type/models'
+
+interface UserMenuProps {
+  token: IToken
+  navigationHandlers: {
+    profile: () => void
+    upgrade: () => void
+    logout: () => void
+  }
+}
+
+const UserMenu = ({ token, navigationHandlers }: UserMenuProps) => {
+  const userMenuItems = [
+    { label: 'Tài khoản', icon: <ProfileIcon />, onClick: navigationHandlers.profile },
+    { label: 'Nâng cấp', icon: <UpgradeIcon />, onClick: navigationHandlers.upgrade },
+    { label: 'Đăng xuất', icon: <LogoutIcon />, onClick: navigationHandlers.logout }
+  ]
+
+  return (
+    <Box position='relative' cursor='pointer' _hover={{ div: { display: 'flex' } }}>
+      <UserCard name={token.user.fullName} role={token.user.role.name.toLocaleLowerCase()} avatar={<UserIcon />} />
+      <Box
+        position='absolute'
+        top='75%'
+        right='0'
+        display='none'
+        bgColor='brand.white'
+        w='150px'
+        borderRadius='md'
+        boxShadow='md'
+        zIndex={10}
+        mt={2}
+        overflow='hidden'
+        border='1px'
+        borderColor='gray.200'
+      >
+        <Flex as='ul' direction='column' w='100%' justifyContent='flex-start'>
+          {userMenuItems.map((item, index) => (
+            <Button
+              as='li'
+              key={index}
+              variant='ghost'
+              size='md'
+              justifyContent='flex-start'
+              py={2}
+              px={4}
+              _hover={{ bgColor: 'gray.200' }}
+              leftIcon={item.icon}
+              fontSize='sm'
+              color='brand.blackTextPrimary'
+              onClick={item.onClick}
+            >
+              {item.label}
+            </Button>
+          ))}
+        </Flex>
+      </Box>
+    </Box>
+  )
+}
+
+export default UserMenu
