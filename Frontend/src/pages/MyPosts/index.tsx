dcdf5cc23aa4cf7e49391d0d46a7abf37bacb83b
DucVan <nguyenducvan260903@gmail.com>
2025-04-02 11:27:07 +0700

    feat: add delete property functionality with confirmation modal #105

--- Frontend/src/pages/MyPosts/index.tsx
+++ Frontend/src/pages/MyPosts/index.tsx
@@ -1,10 +1,10 @@
-import { Box, Flex, Stack } from '@chakra-ui/react'
+import { Box, Flex, Heading, Stack, useDisclosure } from '@chakra-ui/react'
 import { FILTER_OPTION, ITEM_PER_PAGE, SORT_PROPERTY_OPTION } from '@constants/option'
-import { useGetPropertyByUser } from '@hooks'
+import { useCustomToast, useDeleteProperty, useGetPropertyByUser } from '@hooks'
 import { personalPropertyFilterStore } from '@stores'
 import { useShallow } from 'zustand/shallow'
-import { useEffect } from 'react'
-import { CustomTable, Filter, Pagination } from '@components'
+import { FormEvent, useEffect, useMemo, useState } from 'react'
+import { CustomTable, Filter, Pagination, WarningModal } from '@components'
 import { transformPriceUnit } from '@utils'
 import { useNavigate } from 'react-router-dom'
 
@@ -17,7 +17,11 @@ const MyPosts = () => {
   )
   const { setItemsPerPage, setCurrentPage, searchQuery, sortBy, setSearchQuery, setSortBy } =
     personalPropertyFilterStore()
-  const { properties, propertiesQuery, totalProperties, reCallQuery, isError } = useGetPropertyByUser()
+  const { properties, propertiesQuery, totalProperties, isError, infinitePropertyQueryKey } = useGetPropertyByUser()
+  const { deletePropertyMutation } = useDeleteProperty(infinitePropertyQueryKey)
+  const { isOpen: isWarningModalOpen, onOpen: onOpenWarningModal, onClose: onCloseWarningModal } = useDisclosure()
+  const [currentId, setCurrentId] = useState<number | null>(null)
+  const { showToast } = useCustomToast()
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -30,30 +34,63 @@ const MyPosts = () => {
     navigate(`/my-posts/update/${propertyId}`)
   }
 
-  const handleDelete = async (propertyId: number) => {
-    try {
-      console.log(propertyId)
-
-      reCallQuery()
-    } catch (error) {
-      console.error('Error deleting property:', error)
-    }
+  const handleDelete = (propertyId: number) => {
+    setCurrentId(propertyId)
+    onOpenWarningModal()
   }
 
-  const dataTable =
-    properties?.map((property) => ({
-      id: property.id,
-      'Loại hình': FILTER_OPTION.category[property.category.id - 1].label,
-      'Tiêu đề': property.title,
-      'Vị trí': `${property.streetName}, ${property.wardName}, ${property.districtName}, ${property.region}`,
-      'Diện tích': property.area,
-      Giá: transformPriceUnit(property.price),
-      'Trạng thái': property.status,
-      'Cập nhập': new Date(property.createdAt).toLocaleDateString()
-    })) || []
+  const handleCloseWarningModal = () => {
+    setCurrentId(null)
+    onCloseWarningModal()
+  }
+
+  const handleWarningSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!currentId) {
+      showToast({ status: 'error', title: 'Bài viết không tồn tại' })
+      return
+    }
+
+    deletePropertyMutation.mutate(currentId, {
+      onSuccess: (response) => {
+        showToast({
+          status: response.status,
+          title: response.message
+        })
+
+        handleCloseWarningModal()
+      },
+      onError: (error) => {
+        showToast({
+          status: 'error',
+          title: error.message || 'Có lỗi xảy ra khi xóa bài viết'
+        })
+      }
+    })
+  }
+
+  const dataTable = useMemo(() => {
+    const transformedData =
+      properties?.map((property) => ({
+        id: property.id,
+        'Loại hình': FILTER_OPTION.category[property.category.id - 1].label,
+        'Tiêu đề': property.title,
+        'Vị trí': `${property.streetName}, ${property.wardName}, ${property.districtName}, ${property.region}`,
+        'Diện tích': property.area,
+        Giá: transformPriceUnit(property.price),
+        'Trạng thái': property.status,
+        'Cập nhập': new Date(property.createdAt).toLocaleDateString()
+      })) || []
+
+    return transformedData.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage)
+  }, [properties, currentPage, itemsPerPage])
 
   if (isError) {
-    return <Box>Error loading properties. Please try again.</Box>
+    return (
+      <Heading variant='secondary' color='brand.red' p={10}>
+        Không tìm thấy bài viết cá nhân
+      </Heading>
+    )
   }
 
   return (
@@ -88,6 +125,15 @@ const MyPosts = () => {
           />
         </Flex>
       </Box>
+
+      <WarningModal
+        isModalOpen={isWarningModalOpen}
+        onClose={handleCloseWarningModal}
+        title='WARNING'
+        message='Hành động này sẽ xóa vĩnh viễn bài viết. Bạn có muốn tiếp tục không?'
+        handleSubmit={handleWarningSubmit}
+        isSubmitting={deletePropertyMutation.isPending}
+      />
     </Stack>
   )
 }
