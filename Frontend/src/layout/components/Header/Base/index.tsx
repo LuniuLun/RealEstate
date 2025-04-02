c3679554365613718d916e9d64a1774a513dde55
DucVan <nguyenducvan260903@gmail.com>
2025-04-02 14:54:24 +0700

    refactor: simplify BaseHeader by extracting HeaderActions component #109

--- Frontend/src/layout/components/Header/Base/index.tsx
+++ Frontend/src/layout/components/Header/Base/index.tsx
@@ -1,25 +1,22 @@
-import { Box, Button, Flex } from '@chakra-ui/react'
-import { LogoIcon, LogoutIcon, NotificationIcon, ProfileIcon, UserIcon } from '@assets/icons'
-import { Logo, NavItem, UserCard } from '@components'
+import { Flex } from '@chakra-ui/react'
+import { LogoIcon } from '@assets/icons'
+import { HeaderActions, Logo, NavItem } from '@components'
 import { authStore } from '@stores'
 import { useNavigate } from 'react-router-dom'
 
 const BaseHeader = () => {
-  // const { toggleSidebar } = useSidebar()
   const { token, logout } = authStore()
   const navigate = useNavigate()
 
-  const handleLogout = () => {
-    logout()
-    navigate('/')
-  }
-
-  const handleGoToProfile = () => {
-    navigate('/personal')
-  }
-
-  const handleGoToNewProperty = () => {
-    navigate('/new-property')
+  const navigationHandlers = {
+    profile: () => navigate('/personal'),
+    upgrade: () => navigate('/upgrade'),
+    newProperty: () => navigate('/new-property'),
+    login: () => navigate('/login'),
+    logout: () => {
+      logout()
+      navigate('/')
+    }
   }
 
   return (
@@ -40,75 +37,7 @@ const BaseHeader = () => {
         <NavItem title='Trang chủ' to='/' isActive={false} />
         <NavItem title='Bài đăng' to='/property-listings' isActive={false} />
       </Flex>
-      <Flex alignItems='center' gap={5}>
-        {token ? (
-          <Flex alignItems='center' gap={6}>
-            <Box height='100%'>
-              <NotificationIcon />
-            </Box>
-            <Box position='relative' cursor='pointer' _hover={{ div: { display: 'flex' } }}>
-              <UserCard
-                name={token.user.fullName}
-                role={token.user.role.name.toLocaleLowerCase()}
-                avatar={<UserIcon />}
-              />
-              <Box
-                position='absolute'
-                top='75%'
-                right='0'
-                display='none'
-                bgColor='brand.white'
-                w='150px'
-                borderRadius='md'
-                boxShadow='md'
-                zIndex={10}
-                mt={2}
-                overflow='hidden'
-                border='1px'
-                borderColor='gray.200'
-              >
-                <Flex direction='column' w='100%' justifyContent='flex-start'>
-                  <Button
-                    variant='ghost'
-                    size='md'
-                    justifyContent='flex-start'
-                    py={2}
-                    px={4}
-                    _hover={{ bgColor: 'gray.200' }}
-                    leftIcon={<ProfileIcon />}
-                    fontSize='sm'
-                    color='brand.blackTextPrimary'
-                    onClick={handleGoToProfile}
-                  >
-                    Tài khoản
-                  </Button>
-                  <Button
-                    variant='ghost'
-                    size='md'
-                    justifyContent='flex-start'
-                    py={2}
-                    px={4}
-                    _hover={{ bgColor: 'gray.200' }}
-                    leftIcon={<LogoutIcon />}
-                    fontSize='sm'
-                    color='brand.blackTextPrimary'
-                    onClick={handleLogout}
-                  >
-                    Đăng xuất
-                  </Button>
-                </Flex>
-              </Box>
-            </Box>
-          </Flex>
-        ) : (
-          <Button onClick={() => navigate('/login')} colorScheme='brand' variant='tertiary'>
-            Đăng nhập
-          </Button>
-        )}
-        <Button colorScheme='brand' variant='tertiary' onClick={handleGoToNewProperty}>
-          Đăng tin
-        </Button>
-      </Flex>
+      <HeaderActions token={token} navigationHandlers={navigationHandlers} />
     </Flex>
   )
 }
