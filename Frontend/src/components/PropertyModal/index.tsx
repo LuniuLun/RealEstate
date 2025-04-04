4174e9634cc4b16f43cefde3fdec35f890051f4f
DucVan <nguyenducvan260903@gmail.com>
2025-04-04 10:48:41 +0700

    feat: update property modal to handle approval and rejection states #117

--- Frontend/src/components/PropertyModal/index.tsx
+++ Frontend/src/components/PropertyModal/index.tsx
@@ -1,6 +1,6 @@
 import { CustomModal, PropertyDetails } from '@components'
-import { IProperty } from '@type/models'
-import { memo } from 'react'
+import { IProperty, PropertyStatus } from '@type/models'
+import { memo, useMemo } from 'react'
 
 interface IPropertyModalProps {
   isModalOpen: boolean
@@ -23,6 +23,9 @@ const PropertyModal = ({
   isSubmitting,
   isLoading
 }: IPropertyModalProps) => {
+  const isApproved = useMemo(() => property?.status === PropertyStatus.APPROVAL, [property?.status])
+  const isCanceled = useMemo(() => property?.status === PropertyStatus.CANCELED, [property?.status])
+
   return (
     <CustomModal
       size={'lg'}
@@ -31,9 +34,11 @@ const PropertyModal = ({
       title={title}
       handleSubmit={handleSubmit}
       isSubmitting={isSubmitting}
-      titleSubmitButton='Duyệt'
-      titleCancelButton='Từ chối'
+      titleSubmitButton={isApproved ? 'Đã duyệt' : 'Duyệt'}
+      titleCancelButton={isCanceled ? 'Đã từ chối' : 'Từ chối'}
       handleClose={handleClose}
+      isApproveDisabled={isApproved}
+      isRejectDisabled={isCanceled}
     >
       <PropertyDetails property={property} isLoading={isLoading} />
     </CustomModal>
