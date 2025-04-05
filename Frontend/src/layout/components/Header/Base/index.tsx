a229a18705a13253250ff4274b42ad14cb9d5e21
DucVan <nguyenducvan260903@gmail.com>
2025-04-05 20:53:24 +0700

    refactor: remove unused imports and simplify BaseHeader component

--- Frontend/src/layout/components/Header/Base/index.tsx
+++ Frontend/src/layout/components/Header/Base/index.tsx
@@ -1,24 +1,8 @@
 import { Flex } from '@chakra-ui/react'
 import { LogoIcon } from '@assets/icons'
 import { HeaderActions, Logo, NavItem } from '@components'
-import { authStore } from '@stores'
-import { useNavigate } from 'react-router-dom'
 
 const BaseHeader = () => {
-  const { token, logout } = authStore()
-  const navigate = useNavigate()
-
-  const navigationHandlers = {
-    profile: () => navigate('/personal'),
-    upgrade: () => navigate('/upgrade'),
-    newProperty: () => navigate('/new-property'),
-    login: () => navigate('/login'),
-    logout: () => {
-      logout()
-      navigate('/')
-    }
-  }
-
   return (
     <Flex
       as='nav'
@@ -37,7 +21,7 @@ const BaseHeader = () => {
         <NavItem title='Trang chủ' to='/' isActive={false} />
         <NavItem title='Bài đăng' to='/property-listings' isActive={false} />
       </Flex>
-      <HeaderActions token={token} navigationHandlers={navigationHandlers} />
+      <HeaderActions />
     </Flex>
   )
 }
