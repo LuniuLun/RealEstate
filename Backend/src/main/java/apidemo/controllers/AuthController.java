5bf41b2de8bb93172d068a1253ee8da73f97ace8
DucVan <nguyenducvan260903@gmail.com>
2025-04-07 17:54:30 +0700

    chore: remove compact import

--- Backend/src/main/java/apidemo/controllers/AuthController.java
+++ Backend/src/main/java/apidemo/controllers/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import apidemo.models.FavouriteProperty;
 import apidemo.models.LoginRequest;
 import apidemo.models.LogoutRequest;
 import apidemo.models.Token;
