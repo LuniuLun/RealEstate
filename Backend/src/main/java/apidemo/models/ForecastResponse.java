68ddb1eb78208bc724fbfb893eb756fbf437f9dd
DucVan <nguyenducvan260903@gmail.com>
2025-04-13 08:28:57 +0700

    feat: add ForecastResponse model with district, periodDays, and predictions #114

---
+++ Backend/src/main/java/apidemo/models/ForecastResponse.java
@@ -0,0 +1,18 @@
+package apidemo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForecastResponse {
+  private String district;
+  private int periodDays;
+  private List<PricePrediction> predictions;
+}
