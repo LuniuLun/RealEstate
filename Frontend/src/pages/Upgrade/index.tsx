eb5f7c517bcf9aa3620bfafd24c4914433669357
DucVan <nguyenducvan260903@gmail.com>
2025-04-17 15:03:21 +0700

    feat: prevent downgrade to lower pricing plans and display warning message

--- Frontend/src/pages/Upgrade/index.tsx
+++ Frontend/src/pages/Upgrade/index.tsx
@@ -2,16 +2,32 @@ import { Container, Flex, Heading, Text, useDisclosure, VStack } from '@chakra-u
 import { PricingCard, WarningModal } from '@components'
 import { PRICINGPLANS } from '@constants/pricingPlans'
 import { useVNPayMutation } from '@hooks'
-import { PricingPlan } from '@type/upgradePackage'
+import { authStore } from '@stores'
+import { RoleName } from '@type/models'
+import { KindOfUpgradePackage, PricingPlan } from '@type/upgradePackage'
 import { FormEvent, useState } from 'react'
 
 const Upgrade = () => {
   const [selectedPlan, setSelectedPlan] = useState<PricingPlan | null>(null)
+  const [availablePurchase, setAvailablePurchase] = useState(false)
+  const [message, setMessage] = useState<string>()
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { paymentMutation } = useVNPayMutation()
+  const { token } = authStore()
+  const roleValue = token?.user?.role?.name
 
   const handleSelectPlan = (plan: PricingPlan) => {
     setSelectedPlan(plan)
+
+    if (roleValue === RoleName.CUSTOMER && plan.id === KindOfUpgradePackage.BASIC) {
+      setAvailablePurchase(true)
+      setMessage(undefined)
+    } else {
+      setAvailablePurchase(false)
+      if (roleValue !== RoleName.CUSTOMER) {
+        setMessage(`Tài khoản của bạn đã được nâng cấp lên gói ${plan.title} hoặc cao hơn`)
+      }
+    }
     onOpen()
   }
 
@@ -47,9 +63,19 @@ const Upgrade = () => {
       <WarningModal
         isModalOpen={isOpen}
         onClose={onClose}
-        title='Xác nhận nâng cấp'
-        message={`Bạn chắc chắn nâng cấp tài khoản với ${selectedPlan?.title} có giá ${selectedPlan?.price.amount.toLocaleString()} VND không?`}
-        handleSubmit={handleConfirmUpgrade}
+        title={availablePurchase ? 'Xác nhận nâng cấp' : 'Thông báo'}
+        message={
+          message ??
+          `Bạn chắc chắn nâng cấp tài khoản với ${selectedPlan?.title} có giá ${selectedPlan?.price.amount.toLocaleString()} VND không?`
+        }
+        handleSubmit={
+          availablePurchase
+            ? handleConfirmUpgrade
+            : (e) => {
+                e.preventDefault()
+                onClose()
+              }
+        }
         isSubmitting={paymentMutation.isPending}
       />
     </Container>
