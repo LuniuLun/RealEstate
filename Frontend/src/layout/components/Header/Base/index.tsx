4b9db2b1bf1e68260135d78ec2d9616fc06c0125
DucVan <nguyenducvan260903@gmail.com>
2025-04-17 17:05:41 +0700

    feat: conditionally render post nav item based on user role

--- Frontend/src/layout/components/Header/Base/index.tsx
+++ Frontend/src/layout/components/Header/Base/index.tsx
@@ -2,8 +2,11 @@ import { Flex } from '@chakra-ui/react'
 import { LogoIcon } from '@assets/icons'
 import { HeaderActions, Logo, NavItem } from '@components'
 import { memo } from 'react'
+import { RoleName } from '@type/models'
+import useAuthStore from '@stores/Authentication'
 
 const BaseHeader = () => {
+  const { token } = useAuthStore()
   return (
     <Flex
       as='nav'
@@ -20,7 +23,9 @@ const BaseHeader = () => {
       </Flex>
       <Flex>
         <NavItem title='Trang chủ' to='/' isActive={false} />
-        <NavItem title='Bài đăng' to='/property-listings' isActive={false} />
+        {token?.user.role.name !== RoleName.ADMIN && (
+          <NavItem title='Bài đăng' to='/property-listings' isActive={false} />
+        )}
       </Flex>
       <HeaderActions />
     </Flex>
