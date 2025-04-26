757e8b09f81f8b443124c987d6f8c5a4d47adfd5
DucVan <nguyenducvan260903@gmail.com>
2025-04-26 17:46:19 +0700

    feat: expand ForecastRequest model with property and feature fields #124

--- Backend/src/main/java/apidemo/models/ForecastRequest.java
+++ Backend/src/main/java/apidemo/models/ForecastRequest.java
@@ -1,9 +1,27 @@
 package apidemo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ForecastRequest {
   private String district;
+  @Builder.Default
   private Integer periodDays = 90;
+  private Double width;
+  private Double length;
+  private Integer floors;
+  private Integer rooms;
+  private Integer toilets;
+  private List<Integer> landCharacteristics;
+  private Integer categoryId;
+  private Integer directionId;
+  private Integer furnishingId;
 }
