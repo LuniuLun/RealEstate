bd8e3ca8439b4cc0cdb6c06492e1228906466798
DucVan <nguyenducvan260903@gmail.com>
2025-04-29 08:42:47 +0700

    refactor: remove min max value in forecast data

--- Frontend/src/hooks/UseForecastPrice/useTransformForecastedData.ts
+++ Frontend/src/hooks/UseForecastPrice/useTransformForecastedData.ts
@@ -11,17 +11,13 @@ interface UseForecastDataProps {
 interface ForecastDataItem {
   date: string
   'Giá dự đoán': number
-  'Giá thấp nhất': number
-  'Giá cao nhất': number
 }
 
 const useTransformForecastedData = ({ forecastData, viewMode }: UseForecastDataProps) => {
   const formatDailyChartData = (predictions: PricePrediction[]): ForecastDataItem[] => {
     return predictions.map((prediction) => ({
       date: formatDate(new Date(prediction.date)),
-      'Giá dự đoán': prediction.predictedPrice,
-      'Giá thấp nhất': prediction.minPrice,
-      'Giá cao nhất': prediction.maxPrice
+      'Giá dự đoán': prediction.predictedPrice
     }))
   }
 
@@ -30,8 +26,6 @@ const useTransformForecastedData = ({ forecastData, viewMode }: UseForecastDataP
       string,
       {
         predictedSum: number
-        minSum: number
-        maxSum: number
         count: number
       }
     > = {}
@@ -43,23 +37,17 @@ const useTransformForecastedData = ({ forecastData, viewMode }: UseForecastDataP
       if (!monthlyData[monthKey]) {
         monthlyData[monthKey] = {
           predictedSum: 0,
-          minSum: 0,
-          maxSum: 0,
           count: 0
         }
       }
 
       monthlyData[monthKey].predictedSum += prediction.predictedPrice
-      monthlyData[monthKey].minSum += prediction.minPrice
-      monthlyData[monthKey].maxSum += prediction.maxPrice
       monthlyData[monthKey].count += 1
     })
 
     return Object.entries(monthlyData).map(([date, data]) => ({
       date,
-      'Giá dự đoán': Math.round(data.predictedSum / data.count),
-      'Giá thấp nhất': Math.round(data.minSum / data.count),
-      'Giá cao nhất': Math.round(data.maxSum / data.count)
+      'Giá dự đoán': Math.round(data.predictedSum / data.count)
     }))
   }
 
@@ -73,7 +61,7 @@ const useTransformForecastedData = ({ forecastData, viewMode }: UseForecastDataP
   const { minY, maxY } = useMemo(() => {
     if (data.length === 0) return { minY: 0, maxY: 0 }
 
-    const allValues = data.flatMap((d) => [d['Giá dự đoán'], d['Giá thấp nhất'], d['Giá cao nhất']])
+    const allValues = data.flatMap((d) => [d['Giá dự đoán']])
     return {
       minY: Math.min(...allValues),
       maxY: Math.max(...allValues)
