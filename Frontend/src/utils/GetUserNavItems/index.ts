467b1b3a7ff04fd08fd614a7f83dd0aa8a98ee2d
DucVan <nguyenducvan260903@gmail.com>
2025-05-24 17:36:16 +0700

    fix: comment out forecast item insertion for BROKER role

--- Frontend/src/utils/GetUserNavItems/index.ts
+++ Frontend/src/utils/GetUserNavItems/index.ts
@@ -4,7 +4,7 @@ import { RoleName } from '@type/models'
 const getUserNavItems = (roleValue: RoleName | undefined) => {
   const navItems = [...BASE_USER_NAV_ITEMS]
   if (roleValue && roleValue === RoleName.BROKER) {
-    navItems.splice(4, 0, { path: 'forecast', id: 'forecast', title: 'Dự đoán' })
+    // navItems.splice(4, 0, { path: 'forecast', id: 'forecast', title: 'Dự đoán' })
   }
   return navItems
 }
